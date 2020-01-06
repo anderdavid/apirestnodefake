93efbd1379fff441ae6ae0c947c700bbd7df4c81
anderson <anderdavid86@hotmail.com>
2020-01-06 12:42:20 -0500

    Se implementa funcion de autenticacion

--- api.js
+++ api.js
@@ -1,9 +1,35 @@
-var express = require('express')
-var http = require('http')
-var app = express()
-const pool = require('./conexion')
+var express = require('express');
+var http = require('http');
+const pool = require('./conexion');
+var body_parser = require('body-parser');
+
+var app = express();
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended:false}));
+
 
 var users =['oscar','juan','marcos']
+
+app.post('/autenticacion',(req,res)=>{
+  
+  console.log(req.body);
+  
+  var query ="SELECT *FROM user u WHERE u.email ='"+req.body.email+"' AND u.password ='"+req.body.password+"'";
+  var response =null;
+  pool.query(query, (error, user) => {
+    if (error) throw error;
+    console.log(user+" "+user.length)
+    if(user.length<=0){
+      response ={isSuccessful:"false",message:"Invalid email or password"}
+    }else{
+      response ={isSuccessful:"true",message:"Login Successful",user:user[0]}
+    }
+    res.send(JSON.stringify(response)); 
+  });
+
+
+})
+
 
 app.get('/user/:id', (req, res) => {
     
